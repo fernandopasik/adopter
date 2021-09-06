711e5b388dee69fb16dcc132871fe61d635931ea
Fernando Pasik <fernando@pasik.com.ar>
2021-09-06 20:17:01 +0100

    refactor: move list package exports to packages

--- src/lib/packages/__tests__/index.spec.ts
+++ src/lib/packages/__tests__/index.spec.ts
@@ -1,6 +1,7 @@
 import findMissingPackages from '../find-missing-packages.js';
 import getPackageName from '../get-package-name.js';
 import * as packages from '../index.js';
+import listPackageExports from '../list-package-exports.js';
 
 describe('packages', () => {
   it('find missing packages', () => {
@@ -9,5 +10,9 @@ describe('packages', () => {
 
   it('get package name', () => {
     expect(packages.getPackageName).toStrictEqual(getPackageName);
+  });
+
+  it('list package exports', () => {
+    expect(packages.listPackageExports).toStrictEqual(listPackageExports);
   });
 });
--- src/lib/packages/index.ts
+++ src/lib/packages/index.ts
@@ -1,2 +1,3 @@
 export { default as findMissingPackages } from './find-missing-packages.js';
 export { default as getPackageName } from './get-package-name.js';
+export { default as listPackageExports } from './list-package-exports.js';
