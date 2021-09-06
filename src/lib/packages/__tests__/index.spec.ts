7aad06a46261090ad5db48c0f7582de749244e63
Fernando Pasik <fernando@pasik.com.ar>
2021-09-06 20:01:58 +0100

    refactor: move get package name to packages

--- src/lib/packages/__tests__/index.spec.ts
+++ src/lib/packages/__tests__/index.spec.ts
@@ -1,8 +1,13 @@
 import findMissingPackages from '../find-missing-packages.js';
+import getPackageName from '../get-package-name.js';
 import * as packages from '../index.js';
 
 describe('packages', () => {
-  it('has find missing packages', () => {
+  it('find missing packages', () => {
     expect(packages.findMissingPackages).toStrictEqual(findMissingPackages);
+  });
+
+  it('get package name', () => {
+    expect(packages.getPackageName).toStrictEqual(getPackageName);
   });
 });
--- src/lib/packages/index.ts
+++ src/lib/packages/index.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as findMissingPackages } from './find-missing-packages.js';
+export { default as getPackageName } from './get-package-name.js';
