89fa452fa1fa0122209dfb91833037f3e0cf997d
Fernando Pasik <fernando@pasik.com.ar>
2021-09-11 00:34:11 +0100

    feat: run adopter

---
+++ src/adopter.ts
@@ -0,0 +1,3 @@
+import run from './lib/run.js';
+
+await run({ packages: ['react', 'lodash', 'load-json-file'] });
---
+++ src/lib/run.ts
@@ -0,0 +1,39 @@
+import type { ReadonlyDeep } from 'type-fest';
+import buildExportsIndex from './build-exports-index.js';
+import { parseImports } from './imports/index.js';
+import type { Import } from './imports/parse-imports.js';
+import listFiles from './list-files.js';
+import processFiles from './process-files.js';
+
+export interface Options {
+  packages: string[];
+  rootDir?: string;
+  srcMatch?: string[];
+  srcIgnorePatterns?: string[];
+}
+
+const DEFAULT_OPTIONS: Options = {
+  packages: [],
+  rootDir: process.cwd(),
+  srcMatch: ['**/*.[j|t]s'],
+  srcIgnorePatterns: ['/(node_modules|.yarn)/'],
+};
+
+const run = async ({
+  packages,
+  srcMatch,
+}: ReadonlyDeep<Options> = DEFAULT_OPTIONS): Promise<void> => {
+  const exportsIndex = await buildExportsIndex(packages);
+  const imports: Import[] = [];
+
+  processFiles(listFiles(srcMatch), (_filePath, _filename, _content, ast) => {
+    if (typeof ast !== 'undefined') {
+      imports.push(...parseImports(ast));
+    }
+  });
+
+  console.log(imports);
+  console.log(exportsIndex);
+};
+
+export default run;
