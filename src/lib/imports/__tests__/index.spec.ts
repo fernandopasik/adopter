d6fd817e245a419d31737024660be4cd44f8c56a
Fernando Pasik <fernando@pasik.com.ar>
2021-09-18 12:23:47 +0100

    fix: add missing exports

--- src/lib/imports/__tests__/index.spec.ts
+++ src/lib/imports/__tests__/index.spec.ts
@@ -1,8 +1,23 @@
+import areNamedImports from '../are-named-imports.js';
+import getImportModuleNames from '../get-import-module-names.js';
 import * as imports from '../index.js';
+import parseImport from '../parse-import.js';
 import parseImports from '../parse-imports.js';
 
 describe('packages', () => {
-  it('find missing packages', () => {
+  it('are named imports', () => {
+    expect(imports.areNamedImports).toStrictEqual(areNamedImports);
+  });
+
+  it('get import module names', () => {
+    expect(imports.getImportModuleNames).toStrictEqual(getImportModuleNames);
+  });
+
+  it('parse import', () => {
+    expect(imports.parseImport).toStrictEqual(parseImport);
+  });
+
+  it('parse imports', () => {
     expect(imports.parseImports).toStrictEqual(parseImports);
   });
 });
--- src/lib/imports/index.ts
+++ src/lib/imports/index.ts
@@ -1,2 +1,5 @@
+export { default as areNamedImports } from './are-named-imports.js';
+export { default as getImportModuleNames } from './get-import-module-names.js';
+export { default as parseImport } from './parse-import.js';
 export type { Import } from './parse-import.js';
 export { default as parseImports } from './parse-imports.js';
