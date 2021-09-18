defd1f32cefa6c9e76d4c85e90adadcbf375b4e0
Fernando Pasik <fernando@pasik.com.ar>
2021-09-18 19:04:30 +0100

    test: add run tests

---
+++ src/lib/__tests__/run.spec.ts
@@ -0,0 +1,81 @@
+import { listFiles, processFiles } from '../files/index.js';
+import { getPackageExports } from '../packages/index.js';
+import { Coverage, Usage } from '../reports/index.js';
+import run from '../run.js';
+
+jest.mock('globby', () => ({
+  globbySync: jest.fn(),
+}));
+
+jest.mock('../files/index.js');
+jest.mock('../packages/index.js');
+jest.mock('../reports/index.js');
+
+describe('run', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('gets packages exports', async () => {
+    const packages = ['dep1', 'dep2'];
+
+    await run({ packages });
+
+    expect(getPackageExports).toHaveBeenCalledTimes(1);
+    expect(getPackageExports).toHaveBeenCalledWith(packages);
+  });
+
+  it('initializes usage with package exports', async () => {
+    const packages = ['dep1', 'dep2'];
+    const packageExports = new Map();
+
+    (getPackageExports as jest.MockedFunction<typeof getPackageExports>).mockResolvedValueOnce(
+      packageExports,
+    );
+
+    await run({ packages });
+
+    expect(Usage).toHaveBeenCalledTimes(1);
+    expect(Usage).toHaveBeenCalledWith(packageExports);
+  });
+
+  it('lists all files from source match expression', async () => {
+    const packages = ['dep1', 'dep2'];
+    const srcMatch = ['*'];
+
+    await run({ packages, srcMatch });
+
+    expect(listFiles).toHaveBeenCalledTimes(1);
+    expect(listFiles).toHaveBeenCalledWith(srcMatch);
+  });
+
+  it('processes all files', async () => {
+    const packages = ['dep1', 'dep2'];
+    const files = ['*'];
+
+    (listFiles as jest.MockedFunction<typeof listFiles>).mockReturnValueOnce(files);
+
+    await run({ packages });
+
+    expect(processFiles).toHaveBeenCalledTimes(1);
+    expect(processFiles).toHaveBeenCalledWith(files, expect.any(Function));
+  });
+
+  it('prints usage', async () => {
+    const packages = ['dep1', 'dep2'];
+
+    await run({ packages });
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(Usage.prototype.print).toHaveBeenCalledTimes(1);
+  });
+
+  it('prints coverage', async () => {
+    const packages = ['dep1', 'dep2'];
+
+    await run({ packages });
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(Coverage.prototype.print).toHaveBeenCalledTimes(1);
+  });
+});
