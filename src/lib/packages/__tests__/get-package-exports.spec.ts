df1005187abdbe1b93b7bc5266892774b4b84009
Fernando Pasik <fernando@pasik.com.ar>
2021-09-22 01:21:22 +0100

    test: list exports with empty module packages

--- src/lib/packages/__tests__/get-package-exports.spec.ts
+++ src/lib/packages/__tests__/get-package-exports.spec.ts
@@ -1,11 +1,12 @@
 import getPackageExports from '../get-package-exports.js';
+import type { Export } from '../list-package-exports.js';
 import listPackageExports from '../list-package-exports.js';
 
 jest.mock('../list-package-exports.js');
 
 describe('get package exports', () => {
   beforeEach(() => {
-    jest.clearAllMocks();
+    jest.restoreAllMocks();
   });
 
   it('lists exports from each package', async () => {
@@ -17,13 +18,14 @@ describe('get package exports', () => {
   });
 
   it('returns a list of package exports', async () => {
-    const packages = ['dep1', 'dep2', 'dep3'];
+    const packages = ['dep1', 'dep2', 'dep3', 'dep4'];
     const dep1 = [
       { name: 'default', type: 'function' },
       { name: 'moduleA', type: 'function' },
     ];
     const dep2 = [{ name: 'moduleA', type: 'string' }];
-    const dep3 = [
+    const dep3: Export[] = [];
+    const dep4 = [
       { name: 'default', type: 'function' },
       { name: 'moduleB', type: 'object' },
     ];
@@ -31,12 +33,13 @@ describe('get package exports', () => {
     (listPackageExports as jest.MockedFunction<typeof listPackageExports>)
       .mockResolvedValueOnce(dep1)
       .mockResolvedValueOnce(dep2)
-      .mockResolvedValueOnce(dep3);
+      .mockResolvedValueOnce(dep3)
+      .mockResolvedValueOnce(dep4);
 
     const packageExports = await getPackageExports(packages);
 
     expect(Array.from(packageExports.keys())).toStrictEqual(packages);
-    expect(Array.from(packageExports.values())).toStrictEqual([dep1, dep2, dep3]);
+    expect(Array.from(packageExports.values())).toStrictEqual([dep1, dep2, dep3, dep4]);
   });
 
   it('with non installed or non existent packages', async () => {
