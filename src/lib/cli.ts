5447a65f97e7b9df61125765009fc526dbea6130
Fernando Pasik <fernando@pasik.com.ar>
2021-09-23 02:45:46 +0100

    feat: describe command line basic usage

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -2,7 +2,10 @@ import yargs from 'yargs';
 import run from './run.js';
 
 const cli = async (processArgs: readonly string[]): Promise<void> => {
-  const { _: argList, rootDir } = yargs(processArgs).string('rootDir').parseSync();
+  const { _: argList, rootDir } = yargs(processArgs)
+    .usage('$0 [options] package1 [package2] [packageN]')
+    .string('rootDir')
+    .parseSync();
   const packages = argList.map((arg) => String(arg));
 
   await run({ packages, rootDir });
