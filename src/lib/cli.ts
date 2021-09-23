02eb8bedef8063c9bc8aa5254ee49b7ffd684bb5
Fernando Pasik <fernando@pasik.com.ar>
2021-09-23 02:46:31 +0100

    feat: demand at least one package to track

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -4,6 +4,7 @@ import run from './run.js';
 const cli = async (processArgs: readonly string[]): Promise<void> => {
   const { _: argList, rootDir } = yargs(processArgs)
     .usage('$0 [options] package1 [package2] [packageN]')
+    .demandCommand(1, 'You need to provide at least one package to track')
     .string('rootDir')
     .parseSync();
   const packages = argList.map((arg) => String(arg));
