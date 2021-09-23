42e28b1d0db59f06564d46c428e51a18f1135cbe
Fernando Pasik <fernando@pasik.com.ar>
2021-09-23 01:02:05 +0100

    test: config tsconfig to use esm

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export default {
   collectCoverageFrom: ['src/**/*.{j,t}s'],
-  globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json' } },
+  globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json', useESM: true } },
   moduleNameMapper: { '(.*)\\.js': '$1' },
   preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
