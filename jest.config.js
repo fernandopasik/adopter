4f750fc5f3308c1d57dc22ede0442666e4f2b0b7
Fernando Pasik <fernando@pasik.com.ar>
2021-09-23 01:24:47 +0100

    test: ignore bin in coverage until es2022

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 export default {
-  collectCoverageFrom: ['src/**/*.{j,t}s'],
+  collectCoverageFrom: ['src/**/*.{j,t}s', '!src/bin/*'],
   globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json', useESM: true } },
   moduleNameMapper: { '(.*)\\.js': '$1' },
   preset: 'ts-jest/presets/js-with-ts-esm',
