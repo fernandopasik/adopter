186e5ecaa3cccec032c09e9291a23039483e7aa3
Fernando Pasik <fernando@pasik.com.ar>
2021-09-24 20:25:46 +0100

    fix: load package json by reading file

--- src/lib/packages/__tests__/get-package-json.spec.ts
+++ src/lib/packages/__tests__/get-package-json.spec.ts
@@ -1,9 +1,14 @@
+import fs from 'fs';
 import log from 'loglevel';
+import extractPackagePath from '../extract-package-path.js';
 import getPackageJson from '../get-package-json.js';
+import resolvePackage from '../resolve-package.js';
 
+jest.mock('../extract-package-path.js');
 jest.mock('../resolve-package.js', () =>
   jest.fn(async (specifier: string) => Promise.resolve(specifier)),
 );
+jest.mock('fs');
 jest.mock('loglevel');
 jest.mock('nanocolors', () => ({
   yellow: (t: string): string => t,
@@ -13,25 +18,62 @@ describe('get package.json', () => {
   beforeEach(() => {
     jest.resetModules();
     jest.dontMock('typescript');
+    jest.clearAllMocks();
   });
 
-  it('with an existing package', async () => {
-    jest.doMock('typescript/package.json', () => ({
-      name: 'typescript',
-    }));
+  it('resolves from package name', async () => {
+    const packageName = 'typescript';
 
-    const packageJson = await getPackageJson('typescript');
+    await getPackageJson(packageName);
 
-    expect(packageJson).toStrictEqual({
-      name: 'typescript',
-    });
+    expect(resolvePackage).toHaveBeenCalledTimes(1);
+    expect(resolvePackage).toHaveBeenCalledWith(packageName);
+  });
+
+  it('extracts package path from main module url', async () => {
+    const packageName = 'typescript';
+    const packageUrl = '/home/project/node_modules/typescript';
+    const mainModuleUrl = `${packageUrl}/lib/main.js`;
+    (resolvePackage as jest.MockedFunction<typeof resolvePackage>).mockResolvedValueOnce(
+      mainModuleUrl,
+    );
+
+    await getPackageJson(packageName);
+
+    expect(extractPackagePath).toHaveBeenCalledTimes(1);
+    expect(extractPackagePath).toHaveBeenCalledWith(mainModuleUrl, packageName);
+  });
+
+  it('reads package.json file', async () => {
+    const packageName = 'typescript';
+    const packagePath = '/home/project/node_modules/typescript';
+    (extractPackagePath as jest.MockedFunction<typeof extractPackagePath>).mockReturnValueOnce(
+      packagePath,
+    );
+    const spy = jest.spyOn(fs, 'readFileSync');
+
+    await getPackageJson(packageName);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(`${packagePath}/package.json`);
+
+    spy.mockRestore();
+  });
+
+  it('returns the package.json content', async () => {
+    const pkgJson = { name: 'typescript' };
+    const spy = jest.spyOn(fs, 'readFileSync').mockReturnValueOnce(JSON.stringify(pkgJson));
+
+    await expect(getPackageJson('typescript')).resolves.toStrictEqual(pkgJson);
+
+    spy.mockRestore();
   });
 
   it('with a non installed module', async () => {
     const spy = jest.spyOn(log, 'warn').mockImplementation();
     const errorMessage = 'Cannot find module';
-    jest.doMock('typescript/package.json', () => {
-      throw new Error(errorMessage);
+    (resolvePackage as jest.MockedFunction<typeof resolvePackage>).mockRejectedValueOnce({
+      message: errorMessage,
     });
 
     const packageJson = await getPackageJson('typescript');
--- src/lib/packages/get-package-json.ts
+++ src/lib/packages/get-package-json.ts
@@ -1,15 +1,17 @@
+import { readFileSync } from 'fs';
 import log from 'loglevel';
 import { yellow } from 'nanocolors';
-import type { Mutable, PackageJson, ReadonlyDeep } from 'type-fest';
+import type { PackageJson } from 'type-fest';
+import extractPackagePath from './extract-package-path.js';
 import resolvePackage from './resolve-package.js';
 
 const getPackageJson = async (packageName: string): Promise<PackageJson | null> =>
-  resolvePackage(`${packageName}/package.json`)
-    .then(async (packageJsonUrl) => import(packageJsonUrl) as Promise<{ default: PackageJson }>)
-    .then(
-      ({ default: packageJson }: ReadonlyDeep<{ default: PackageJson }>) =>
-        packageJson as Mutable<PackageJson>,
-    )
+  resolvePackage(`${packageName}`)
+    .then((mainModuleUrl) => {
+      const packagePath = extractPackagePath(mainModuleUrl, packageName);
+      const packageJsonPath = `${packagePath}/package.json`;
+      return JSON.parse(readFileSync(packageJsonPath).toString()) as PackageJson;
+    })
     .catch((error) => {
       const { message } = error as { message: string };
       log.warn(yellow(message));
