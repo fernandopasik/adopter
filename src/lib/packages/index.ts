77adced3aa5a1879b31507f4683f175a5fde4e9b
Fernando Pasik <fernando@pasik.com.ar>
2021-09-24 02:33:28 +0100

    refactor: deprecate package util

--- src/lib/packages/index.ts
+++ src/lib/packages/index.ts
@@ -2,5 +2,4 @@ export { default as extractPackageName } from './extract-package-name.js';
 export { default as filterTrackedDependencies } from './filter-tracked-dependencies.js';
 export { default as getPackageJson } from './get-package-json.js';
 export { default as getPackageModules } from './get-package-mods.js';
-export { default as getPackagesExports, PackagesExports } from './get-packages-exports.js';
 export { default as listPackageExports, Export } from './list-package-exports.js';
