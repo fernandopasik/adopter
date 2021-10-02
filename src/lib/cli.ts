6869a97d12cc54176b056ad64e03e6f5c2482f97
Fernando Pasik <fernando@pasik.com.ar>
2021-10-02 10:15:42 +0100

    refactor: cli options

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,36 +1,44 @@
+import type { Options } from 'yargs';
 import yargs from 'yargs';
 import run from './run.js';
 
 const MAX_FORMAT_WIDTH = 120;
 
-const DESC: Record<string, string> = {
-  coverage: 'Report file coverage',
-  rootDir: 'Root directory containing files for tracking packages',
-  srcIgnoreMatch: 'Glob patterns to ignore files for tracking packages',
-  srcMatch: 'Glob patterns to match files for tracking packages',
+const OPTIONS: Record<string, Options> = {
+  coverage: {
+    describe: 'Report file coverage',
+    default: false,
+    type: 'boolean',
+  },
+  rootDir: {
+    describe: 'Root directory containing files for tracking packages',
+    default: '.',
+    type: 'string',
+  },
+  srcIgnoreMatch: {
+    describe: 'Glob patterns to ignore files for tracking packages',
+    default: [],
+    type: 'array',
+  },
+  srcMatch: {
+    describe: 'Glob patterns to match files for tracking packages',
+    default: ['**/*.[jt]s?(x)'],
+    type: 'array',
+  },
 };
 
 const cli = async (processArgs: readonly string[]): Promise<void> => {
-  const {
-    _: argList,
-    coverage,
-    rootDir,
-    srcIgnoreMatch,
-    srcMatch,
-  } = yargs(processArgs)
+  const { _: argList, ...options } = yargs(processArgs)
     .parserConfiguration({ 'greedy-arrays': false })
     .usage('$0 [options] package1 [package2] [packageN]')
     .demandCommand(1, 'You need to provide at least one package to track')
-    .option('coverage', { describe: DESC.coverage, default: false, type: 'boolean' })
-    .option('rootDir', { describe: DESC.rootDir, default: '.', type: 'string' })
-    .option('srcIgnoreMatch', { describe: DESC.srcMatch, default: [], type: 'array' })
-    .option('srcMatch', { describe: DESC.srcMatch, default: ['**/*.[jt]s?(x)'], type: 'array' })
+    .options(OPTIONS)
     .wrap(MAX_FORMAT_WIDTH)
     .parseSync();
 
   const packages = argList.map((arg) => String(arg));
 
-  await run({ coverage, packages, rootDir, srcIgnoreMatch, srcMatch });
+  await run({ packages, ...options });
 };
 
 export default cli;
