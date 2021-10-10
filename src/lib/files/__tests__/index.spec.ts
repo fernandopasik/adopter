914aedb6064b5049d4f3a4addab5a6699463d678
Fernando Pasik <fernando@pasik.com.ar>
2021-10-10 15:28:32 +0100

    refactor: remove unused index exports

--- src/lib/files/__tests__/index.spec.ts
+++ src/lib/files/__tests__/index.spec.ts
@@ -1,6 +1,5 @@
 import * as imports from '../index.js';
 import listFiles from '../list-files.js';
-import parseAst from '../parse-ast.js';
 import processFiles from '../process-files.js';
 
 jest.mock('../../packages/resolve-package.js', () => jest.fn((specifier: string) => specifier));
@@ -12,10 +11,6 @@ jest.mock('globby', () => ({
 describe('files', () => {
   it('list files from glob', () => {
     expect(imports.listFiles).toStrictEqual(listFiles);
-  });
-
-  it('parse file AST', () => {
-    expect(imports.parseAst).toStrictEqual(parseAst);
   });
 
   it('process files', () => {
--- src/lib/files/index.ts
+++ src/lib/files/index.ts
@@ -1,3 +1,2 @@
 export { default as listFiles } from './list-files.js';
-export { default as parseAst } from './parse-ast.js';
 export { default as processFiles } from './process-files.js';
