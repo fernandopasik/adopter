61fe1ed5a80bbfecae4e4a72fd2c9421956fd77d
Fernando Pasik <fernando@pasik.com.ar>
2021-10-10 15:26:51 +0100

    refactor: remove unused index exports

--- src/lib/imports/__tests__/index.spec.ts
+++ src/lib/imports/__tests__/index.spec.ts
@@ -1,24 +1,9 @@
-import areNamedImports from '../are-named-imports.js';
-import getImportModuleNames from '../get-import-module-names.js';
 import * as imports from '../index.js';
-import parseImport from '../parse-import.js';
 import parseImports from '../parse-imports.js';
 
 jest.mock('../../packages/resolve-package.js', () => jest.fn((specifier: string) => specifier));
 
 describe('packages', () => {
-  it('are named imports', () => {
-    expect(imports.areNamedImports).toStrictEqual(areNamedImports);
-  });
-
-  it('get import module names', () => {
-    expect(imports.getImportModuleNames).toStrictEqual(getImportModuleNames);
-  });
-
-  it('parse import', () => {
-    expect(imports.parseImport).toStrictEqual(parseImport);
-  });
-
   it('parse imports', () => {
     expect(imports.parseImports).toStrictEqual(parseImports);
   });
