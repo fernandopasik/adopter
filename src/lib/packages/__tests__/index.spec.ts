def0dba10567ebed34d40384cdfda8526d2090cf
Fernando Pasik <fernando@pasik.com.ar>
2021-10-10 15:35:03 +0100

    refactor: add new package methods to index

--- src/lib/packages/__tests__/index.spec.ts
+++ src/lib/packages/__tests__/index.spec.ts
@@ -1,12 +1,18 @@
+import analyzePackages from '../analyze-packages.js';
 import extractPackagePath from '../extract-package-path.js';
 import filterTrackedDependencies from '../filter-tracked-dependencies.js';
 import getPackageJson from '../get-package-json.js';
 import getPackageModules from '../get-package-mods.js';
 import * as packages from '../index.js';
+import { getPackage, getPackageNames, hasModule, hasPackage } from '../packages.js';
 
 jest.mock('../resolve-package.js', () => jest.fn((specifier: string) => specifier));
 
 describe('packages', () => {
+  it('analyze packages', () => {
+    expect(packages.analyzePackages).toStrictEqual(analyzePackages);
+  });
+
   it('extract package path', () => {
     expect(packages.extractPackagePath).toStrictEqual(extractPackagePath);
   });
@@ -15,11 +21,27 @@ describe('packages', () => {
     expect(packages.filterTrackedDependencies).toStrictEqual(filterTrackedDependencies);
   });
 
+  it('get package', () => {
+    expect(packages.getPackage).toStrictEqual(getPackage);
+  });
+
   it('get package json', () => {
     expect(packages.getPackageJson).toStrictEqual(getPackageJson);
   });
 
   it('get package mods', () => {
     expect(packages.getPackageModules).toStrictEqual(getPackageModules);
+  });
+
+  it('get package names', () => {
+    expect(packages.getPackageNames).toStrictEqual(getPackageNames);
+  });
+
+  it('has module', () => {
+    expect(packages.hasModule).toStrictEqual(hasModule);
+  });
+
+  it('has package', () => {
+    expect(packages.hasPackage).toStrictEqual(hasPackage);
   });
 });
--- src/lib/packages/index.ts
+++ src/lib/packages/index.ts
@@ -1,4 +1,7 @@
+export { default as analyzePackages } from './analyze-packages.js';
 export { default as extractPackagePath } from './extract-package-path.js';
 export { default as filterTrackedDependencies } from './filter-tracked-dependencies.js';
 export { default as getPackageJson } from './get-package-json.js';
 export { default as getPackageModules } from './get-package-mods.js';
+export { getPackage, getPackageNames, hasModule, hasPackage } from './packages.js';
+export type { Package } from './packages.js';
