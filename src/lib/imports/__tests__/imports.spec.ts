c5fb5469d73f9ed89089a5ac84e89fadc42b85e2
Fernando Pasik <fernando@pasik.com.ar>
2021-10-10 18:27:11 +0100

    feat: store imports

---
+++ src/lib/imports/__tests__/imports.spec.ts
@@ -0,0 +1,77 @@
+import { addImport, getImport, importKey, imports } from '../imports.js';
+
+describe('imports', () => {
+  beforeEach(() => {
+    imports.clear();
+  });
+
+  describe('key', () => {
+    it('with default module in package', () => {
+      const imprt = {
+        moduleNames: ['default'],
+        moduleSpecifier: 'dep1',
+        packageName: 'dep1',
+      };
+
+      expect(importKey(imprt)).toBe('dep1**default');
+    });
+
+    it('with named modules in package', () => {
+      const imprt = {
+        moduleNames: ['methodA', 'methodB'],
+        moduleSpecifier: 'dep1',
+        packageName: 'dep1',
+      };
+
+      expect(importKey(imprt)).toBe('dep1**methodA**methodB');
+    });
+
+    it('with default and  modules in package', () => {
+      const imprt = {
+        moduleNames: ['default', 'methodA', 'methodB'],
+        moduleSpecifier: 'dep1',
+        packageName: 'dep1',
+      };
+
+      expect(importKey(imprt)).toBe('dep1**default**methodA**methodB');
+    });
+
+    it('with a relative import', () => {
+      const imprt = {
+        moduleNames: ['default'],
+        moduleSpecifier: './src/example.ts',
+        packageName: null,
+      };
+
+      expect(importKey(imprt)).toBe('./src/example.ts**default');
+    });
+  });
+
+  it('can add an import', () => {
+    const imprt = {
+      moduleNames: ['default'],
+      moduleSpecifier: 'dep1',
+      packageName: 'dep1',
+    };
+
+    addImport(imprt);
+
+    expect(imports.get('dep1**default')).toStrictEqual(imprt);
+  });
+
+  it('can get an existing import', () => {
+    const imprt = {
+      moduleNames: ['default'],
+      moduleSpecifier: 'dep1',
+      packageName: 'dep1',
+    };
+
+    addImport(imprt);
+
+    expect(getImport('dep1**default')).toStrictEqual(imprt);
+  });
+
+  it('can not get a non xisting import', () => {
+    expect(getImport('dep1**default')).toBeUndefined();
+  });
+});
--- src/lib/imports/imports.ts
+++ src/lib/imports/imports.ts
@@ -1,3 +1,5 @@
+import type { Mutable, ReadonlyDeep } from 'type-fest';
+
 export interface Import {
   defaultName?: string;
   moduleNames: string[];
@@ -5,3 +7,14 @@ export interface Import {
   named?: Record<string, string>;
   packageName: string | null;
 }
+
+export const imports: Map<string, Import> = new Map();
+
+export const importKey = (imprt: ReadonlyDeep<Import>): string =>
+  `${imprt.moduleSpecifier}**${imprt.moduleNames.join('**')}`;
+
+export const addImport = (imprt: ReadonlyDeep<Import>): void => {
+  imports.set(importKey(imprt), imprt as Mutable<Import>);
+};
+
+export const getImport = (key: string): Import | undefined => imports.get(key);
