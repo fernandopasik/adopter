0e405e8cc38e46d737a418ae70b362ce5048c495
Fernando Pasik <fernando@pasik.com.ar>
2021-11-13 04:26:21 +0000

    feat: is package used

---
+++ src/lib/reports/usage/__tests__/is-package-used.spec.ts
@@ -0,0 +1,86 @@
+import type { Import } from '../../../imports/index.js';
+import type { Package } from '../../../packages/index.js';
+import { getPackage, isPackageImported } from '../../../packages/index.js';
+import isPackageUsed from '../is-package-used.js';
+
+jest.mock('../../../packages/index.js', () => ({
+  getPackage: jest.fn(),
+  isPackageImported: jest.fn(() => false),
+}));
+
+describe('is package used', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const pkg = {
+    name: 'example',
+    isInstalled: true,
+    dependants: new Set<Package>(),
+    dependencies: new Set<Package>(),
+    imports: new Set<Import>(),
+    modules: new Set<string>(),
+  };
+
+  const pkg2 = {
+    ...pkg,
+    name: 'example2',
+  };
+
+  it('with a non imported package', () => {
+    (getPackage as jest.MockedFunction<typeof getPackage>).mockReturnValueOnce(pkg);
+    (isPackageImported as jest.MockedFunction<typeof isPackageUsed>).mockReturnValueOnce(false);
+
+    expect(isPackageUsed('example')).toBe(false);
+    expect(isPackageImported).toHaveBeenCalledTimes(1);
+    expect(getPackage).toHaveBeenCalledTimes(1);
+  });
+
+  it('with an imported package', () => {
+    (isPackageImported as jest.MockedFunction<typeof isPackageUsed>).mockReturnValueOnce(true);
+
+    expect(isPackageUsed('example')).toBe(true);
+    expect(isPackageImported).toHaveBeenCalledTimes(1);
+    expect(getPackage).not.toHaveBeenCalled();
+  });
+
+  it('with an imported dependant package', () => {
+    (getPackage as jest.MockedFunction<typeof getPackage>).mockReturnValueOnce({
+      ...pkg,
+      dependants: new Set([pkg2]),
+    });
+    (isPackageImported as jest.MockedFunction<typeof isPackageUsed>).mockReturnValueOnce(false);
+    (isPackageImported as jest.MockedFunction<typeof isPackageUsed>).mockReturnValueOnce(true);
+
+    expect(isPackageUsed('example')).toBe(true);
+    expect(isPackageImported).toHaveBeenCalledTimes(2);
+  });
+
+  it('with not imported dependant packages', () => {
+    (getPackage as jest.MockedFunction<typeof getPackage>).mockReturnValueOnce({
+      ...pkg,
+      dependants: new Set([pkg2]),
+    });
+    (getPackage as jest.MockedFunction<typeof getPackage>).mockReturnValueOnce(pkg2);
+    (isPackageImported as jest.MockedFunction<typeof isPackageUsed>).mockReturnValueOnce(false);
+    (isPackageImported as jest.MockedFunction<typeof isPackageUsed>).mockReturnValueOnce(false);
+
+    expect(isPackageUsed('example')).toBe(false);
+    expect(isPackageImported).toHaveBeenCalledTimes(2);
+    expect(getPackage).toHaveBeenCalledTimes(2);
+  });
+
+  it('with a non tracked dependant packages', () => {
+    (getPackage as jest.MockedFunction<typeof getPackage>).mockReturnValueOnce({
+      ...pkg,
+      dependants: new Set([pkg2]),
+    });
+    (getPackage as jest.MockedFunction<typeof getPackage>).mockReturnValueOnce(undefined);
+    (isPackageImported as jest.MockedFunction<typeof isPackageUsed>).mockReturnValueOnce(false);
+    (isPackageImported as jest.MockedFunction<typeof isPackageUsed>).mockReturnValueOnce(false);
+
+    expect(isPackageUsed('example')).toBe(false);
+    expect(isPackageImported).toHaveBeenCalledTimes(2);
+    expect(getPackage).toHaveBeenCalledTimes(2);
+  });
+});
---
+++ src/lib/reports/usage/is-package-used.ts
@@ -0,0 +1,19 @@
+import { getPackage, isPackageImported } from '../../packages/index.js';
+
+const isPackageUsed = (packageName: string): boolean => {
+  if (isPackageImported(packageName)) {
+    return true;
+  }
+
+  const pkg = getPackage(packageName);
+
+  if (typeof pkg === 'undefined') {
+    return false;
+  }
+
+  return Boolean(
+    Array.from(pkg.dependants).find(({ name }: Readonly<{ name: string }>) => isPackageUsed(name)),
+  );
+};
+
+export default isPackageUsed;
