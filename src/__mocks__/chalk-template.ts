7a721c6855360f141b5e252ec01160cea4d513af
Fernando Pasik <fernando@pasik.com.ar>
2021-11-13 08:19:51 +0000

    fix: generic template literal function mock

--- src/__mocks__/chalk-template.ts
+++ src/__mocks__/chalk-template.ts
@@ -1,1 +1,11 @@
-export default (text: string): string => text;
+export default (texts: readonly string[], ...vars: readonly string[]): string => {
+  let result = '';
+
+  texts.forEach((text, index) => {
+    result += text;
+    // eslint-disable-next-line security/detect-object-injection
+    result += vars[index];
+  });
+
+  return result;
+};
