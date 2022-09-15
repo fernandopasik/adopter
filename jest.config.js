c19d1cc9887e59a5e1eb8bf66728e205ae0b6e61
fernandopasik <fernando@pasik.com.ar>
2022-09-15 15:04:29 +0100

    test: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
 export default {
-  collectCoverageFrom: ['src/**/*.{j,t}s', '!src/bin/*', '!src/lib/packages/resolve-package.ts'],
-  globals: { 'ts-jest': { useESM: true } },
+  collectCoverageFrom: ['src/**/*.{j,t}s'],
   moduleNameMapper: { '(.*)\\.js': '$1' },
   preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
