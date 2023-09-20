1388f871f1004d461961d54ecd922f6cf4694966
Fernando Pasik <fernando@pasik.com.ar>
2023-09-21 00:17:41 +0100

    style: ignore rule for now in line

--- src/lib/packages/resolve-package.ts
+++ src/lib/packages/resolve-package.ts
@@ -1,11 +1,12 @@
 import { pathToFileURL } from 'url';
 
+// eslint-disable-next-line @typescript-eslint/require-await
 const resolvePackage = async (specifier: string): Promise<string> => {
   const cwd = `${process.cwd()}/`;
   const cwdUrl = pathToFileURL(cwd).href;
 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  return import.meta.resolve!(specifier, cwdUrl);
+  return import.meta.resolve(specifier, cwdUrl);
 };
 
 export default resolvePackage;
