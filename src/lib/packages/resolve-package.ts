404e112fd035d2d707434808347bbc54030c138a
Fernando Pasik <fernando@pasik.com.ar>
2024-06-07 22:31:45 +0100

    style: remove unused disable rule

--- src/lib/packages/resolve-package.ts
+++ src/lib/packages/resolve-package.ts
@@ -5,7 +5,6 @@ const resolvePackage = async (specifier: string): Promise<string> => {
   const cwd = `${process.cwd()}/`;
   const cwdUrl = pathToFileURL(cwd).href;
 
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   return import.meta.resolve(specifier, cwdUrl);
 };
 
