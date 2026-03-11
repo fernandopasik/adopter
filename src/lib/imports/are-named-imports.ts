90a318aee9464fccbf7729c1865c8b22ed9d9eba
Fernando Pasik <fernando@pasik.com.ar>
2026-03-11 18:09:36 +0000

    refactor: remove named import

--- src/lib/imports/are-named-imports.ts
+++ src/lib/imports/are-named-imports.ts
@@ -1,8 +1,6 @@
 import { isNamedImports, type NamedImportBindings, type NamedImports } from 'typescript';
 
-export const areNamedImports = (
-  namedBindings?: NamedImportBindings,
-): namedBindings is NamedImports =>
+const areNamedImports = (namedBindings?: NamedImportBindings): namedBindings is NamedImports =>
   typeof namedBindings !== 'undefined' && isNamedImports(namedBindings);
 
 export default areNamedImports;
